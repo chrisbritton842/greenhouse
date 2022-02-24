ce32761dbdf58764aced5f59349f0b477f070127
Jesse Christensen <jtq.christensen@gmail.com>
2022-02-24 13:56:11 -0800

    phase 3

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ThemeProvider from './context/ThemeContext';
+import ClimateContext from './context/ClimateContext';
 
 function Root() {
   return (
+    <ClimateContext>
       <ThemeProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </ThemeProvider>
+    </ClimateContext>
   );
 }
 
