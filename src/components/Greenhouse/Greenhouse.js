59dfc5624f5104cfd212cc8c85b6815db3598036
Jesse Christensen <jtq.christensen@gmail.com>
2022-02-24 13:01:39 -0800

    Phase 1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -5,12 +5,19 @@ import './Greenhouse.css';
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
+import { useContext } from 'react';
+import { ThemeContext, useTheme} from '../../context/ThemeContext';
+
 function Greenhouse() {
+
+  const context = useContext(ThemeContext);
+
+  const theme = useTheme(context);
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={theme.themeName === 'day' ? dayImage : nightImage}
             alt='greenhouse' 
       />
       <LightSwitch />
